dd8a9ec26632b563ebfbe0b9ee45a84761c46f11
Philip Mørch <mrphil2105@gmail.com>
2026-04-23 15:02:04 +0200

    feat(nix): disable auto start of router VPN

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -6,12 +6,8 @@
   ];
   networking.hostName = "mrphil2105-NixLaptop";
   services.power-profiles-daemon.enable = true;
-  services.openvpn.servers = {
-    router = {
-      config = "config /home/mrphil2105/.openvpn/router.ovpn";
-      autoStart = true;
-    };
-  };
+  services.openvpn.servers.router.config = "config /home/mrphil2105/.openvpn/router.ovpn";
+  services.openvpn.servers.router.autoStart = false;
   services.tailscale.enable = true;
   # https://kisonecat.com/blog/eduroam-openssl-wpa-supplicant/
   nixpkgs.config.packageOverrides = pkgs: {
