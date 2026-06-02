cc722a2220228011a5fedf123466c5e8202d296f
Philip Mørch <mrphil2105@gmail.com>
2026-06-02 23:03:22 +0200

    feat(nix): use latest version of Nvidia driver

--- hosts/desktop/nvidia.nix
+++ hosts/desktop/nvidia.nix
@@ -6,7 +6,7 @@
     powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
   services.xserver.videoDrivers = [ "nvidia" ];
   services.lact.enable = true;
