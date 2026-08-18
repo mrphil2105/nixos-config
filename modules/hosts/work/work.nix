54dbd4efe56340baf7567a9803a037e3fa3b0b8b
Philip Mørch <mrphil2105@gmail.com>
2026-08-18 18:19:39 +0200

    wip: intune 2

--- modules/hosts/work/work.nix
+++ modules/hosts/work/work.nix
@@ -4,7 +4,7 @@
       self.modules.nixos.work
     ];
   };
-  flake.modules.nixos.work = { ... }: {
+  flake.modules.nixos.work = { config, pkgs, ... }: {
     imports = [
       self.modules.nixos.general
       ./_hardware.nix
@@ -16,6 +16,31 @@
       tailscale.enable = true;
       intune.enable = true;
       gnome.glib-networking.enable = true;
+    };
+    environment.systemPackages = with pkgs; [
+      seahorse
+      libsecret
+    ];
+    systemd.services.display-manager.serviceConfig.KeyringMode = "inherit";
+    security.pam.services.sddm-autologin.rules = {
+      auth = {
+        systemd_loadkey = {
+          order = config.security.pam.services.sddm-autologin.rules.auth."sddm-autologin-user".order + 10;
+          control = "optional";
+          modulePath = "${config.systemd.package}/lib/security/pam_systemd_loadkey.so";
+        };
+        gnome_keyring = {
+          order = config.security.pam.services.sddm-autologin.rules.auth."sddm-autologin-user".order + 20;
+          control = "optional";
+          modulePath = "${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so";
+        };
+      };
+      session.gnome_keyring = {
+        order = config.security.pam.services.sddm-autologin.rules.session.sddm.order + 10;
+        control = "optional";
+        modulePath = "${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so";
+        settings.auto_start = true;
+      };
     };
     nixpkgs.overlays = [
       (final: prev: {
@@ -41,11 +66,16 @@
     wayland.windowManager.hyprland.settings = {
       monitor = [
         "eDP-1, 1920x1200, 0x0, 1"
+        "desc:Dell Inc. DELL S2722DC 7C9MHD3, 2560x1440, -1600x-1440, 1"
+        "desc:Dell Inc. DELL S2725DC 2XNKPC4, 2560x1440, 960x-1440, 1"
       ];
-      workspace = map (i: "${toString i}, monitor:eDP-1") (lib.range 1 10);
-      exec-once = [
-        "slack --startup &"
-      ];
+      workspace =
+        (map (i: "${toString i}, monitor:eDP-1") (lib.range 1 10))
+        ++ (map (i: "${toString i}, monitor:desc:Dell Inc. DELL S2722DC 7C9MHD3") (lib.range 11 13))
+        ++ (map (i: "${toString i}, monitor:desc:Dell Inc. DELL S2725DC 2XNKPC4") (lib.range 14 15));
+      # exec-once = [
+      #   "slack --startup &"
+      # ];
       windowrule = [
         "match:class slack, workspace 6"
       ];
