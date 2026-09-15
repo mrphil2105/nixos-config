a8eb55b6404a9359ec2b376f1b4df980e13e1633
Philip Mørch <mrphil2105@gmail.com>
2026-09-15 21:17:06 +0200

    feat(nix): enable weekly garbage collection

--- modules/core.nix
+++ modules/core.nix
@@ -8,12 +8,19 @@
       system
       user
     ];
-    nix.settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      auto-optimise-store = true;
+    nix = {
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        auto-optimise-store = true;
+      };
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
     };
     nixpkgs.config.allowUnfree = true;
     boot = {
